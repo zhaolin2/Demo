a4521d5f34f2d3352c052a2c028d55b13027d001
Administrator <1548021143@qq.com>
2020-09-24 00:14:18 +0800

    添加回调相关

---
+++ CommonDemo/src/main/java/com/util/ThreadPoolUtil.java
@@ -0,0 +1,15 @@
+package com.util;
+
+import sun.nio.ch.ThreadPool;
+
+/**
+ * @Description:
+ * @Author: zl
+ * @date: 2020/9/22
+ */
+public class ThreadPoolUtil {
+
+    private ThreadPoolUtil(){
+
+    }
+}
